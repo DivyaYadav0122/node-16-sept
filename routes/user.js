4a16b2f9ea88a100e0d3a081cb38e63a2610eaf8
Divyayadav0122 <divyay414@gmail.com>
2024-09-16 12:47:05 +0530

    (change in user route...)

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,7 @@ const router = express.Router();
 router.get('/users',(req,res)=>{
     userscontroler.getUser(req,res);
 })
+router.get('/user/:id',(req,res)=>{
+    userscontroler.getParticularUser(req,res)
+})
 module.exports = router;
